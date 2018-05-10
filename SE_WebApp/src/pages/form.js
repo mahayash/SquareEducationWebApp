1076cd6e489fe67ca50cfc08d87eee02fa3b46ec
yashaswi maharshi <maha.yash@gmail.com>
2018-05-10 20:22:00 +0530

    add the clear your doubt form

--- SE_WebApp/src/pages/form.js
+++ SE_WebApp/src/pages/form.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
+import '../styles/form.css'
+
 export default function Form() {
   return (
     <div>
       <Helmet title={`Form`} />
-      <div>
-        <h1>This is my Form Page</h1>
+      <div id="#formMain">
+        <input id="tab1" type="radio" name="tabs" defaultChecked />
+        <label htmlFor="tab1">Clear Your Doubt</label>
+
+        <input id="tab2" type="radio" name="tabs" />
+        <label htmlFor="tab2">Suggestion</label>
+
+        <input id="tab3" type="radio" name="tabs" />
+        <label htmlFor="tab3">Complaint</label>
+
+        <input id="tab4" type="radio" name="tabs" />
+        <label htmlFor="tab4">Submit Marks</label>
+
+        <section id="content1">
+          <iframe style={{width:'100%', height:'520px'}}
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfQPwVwo1ypbufJIquj28Ip0XgW--_NW_tMu6qDhZ8LmNDhJQ/viewform?embedded=true"
+          >
+            Loading...
+          </iframe>
+        </section>
+
+        <section id="content2">
+          <h1>Content 2</h1>
+        </section>
+
+        <section id="content3">
+          <h1>Content 3</h1>
+        </section>
+
+        <section id="content4">
+          <h1>Content 4</h1>
+        </section>
       </div>
     </div>
   )
